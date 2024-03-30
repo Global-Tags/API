e59b72cf9ef1443654e44aa41236b4dddf8ddb70
RappyTV <rappytv@rappytv.com>
2024-03-30 16:40:07 +0100

    Let logging function log anything instead of only strings

--- src/libs/Logger.ts
+++ src/libs/Logger.ts
@@ -1,15 +1,15 @@
 import chalk from "chalk";
 
 export default class Logger {
-    public static info(text: string) {
+    public static info(text: any) {
         console.log(chalk.blue(`[INFO]`), text);
     }
 
-    public static warn(text: string) {
+    public static warn(text: any) {
         console.log(chalk.yellow(`[WARN]`), text);
     }
 
-    public static error(text: string) {
+    public static error(text: any) {
         console.log(chalk.red(`[ERROR]`), text);
     }
 }
