8c6736e9dfd6aa8e9c696a137758c753c3cd5936
RappyTV <rappytv@rappytv.com>
2024-03-30 17:13:37 +0100

    Load routes automatically

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { Elysia } from "elysia";
 import Logger from "./libs/Logger";
 import * as config from "../config.json";
-import banRoute from "./routes/ban";
-import iconRoute from "./routes/icon";
-import positionRoute from "./routes/position";
-import reportRoute from "./routes/report";
-import rootRoute from "./routes/root";
 import { connect } from "./database/mongo";
+import { readdirSync } from "fs";
+import { join } from "path";
 
 // Database connection
 connect(config.srv);
@@ -15,10 +12,17 @@ connect(config.srv);
 export const api = new Elysia({
     prefix: "/players/:uuid"
 })
-.use(banRoute)
-.use(iconRoute)
-.use(positionRoute)
-.use(reportRoute)
-.use(rootRoute)
 .onStart(() => Logger.info(`Elysia listening on port ${config.port}!`))
 .listen(config.port);
+
+loadRoutes();
+
+async function loadRoutes() {
+    const directory = join(__dirname, 'routes')
+    for(const file of readdirSync(directory)) {
+        const route: Elysia = (await import(join(directory, file))).default;
+    
+        api.use(route);
+        Logger.info(`Loaded route /${file != `root.ts` ? file.slice(0, -3) : ``}`);
+    }
+}
