144b41a5ab212b6cc035cbb443d57d5f5fe8c0a6
RappyTV <rappytv@rappytv.com>
2024-03-30 16:04:21 +0100

    Add colors to log messages

--- src/libs/Logger.ts
+++ src/libs/Logger.ts
@@ -1,13 +1,15 @@
+import chalk from "chalk";
+
 export default class Logger {
     public static info(text: string) {
-        console.log(`[INFO]`, text);
+        console.log(chalk.blue(`[INFO]`), text);
     }
 
     public static warn(text: string) {
-        console.log(`[WARN]`, text);
+        console.log(chalk.yellow(`[WARN]`), text);
     }
 
     public static error(text: string) {
-        console.log(`[ERROR]`, text);
+        console.log(chalk.red(`[ERROR]`), text);
     }
 }
