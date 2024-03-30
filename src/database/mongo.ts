f6a1677acdc3c9d1ab35f73fc6a0c027dc5a65aa
RappyTV <rappytv@rappytv.com>
2024-03-30 20:41:58 +0100

    Add error handling to database connection

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 import Logger from "../libs/Logger";
 
 export async function connect(srv: string) {
-    return await mongoose.connect(srv).then(() => {
+    return await mongoose.connect(srv)
+    .then(() => {
         Logger.info("Connected to MongoDB!");
+    }).catch((err) => {
+        Logger.error(`Failed to establish database connection! ${err}`);
+        setTimeout(() => connect(srv), 5000);
     });
 }
 
