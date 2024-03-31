19b4b15cdc2e85b7bfc0f37580ccd2c010d5321c
RappyTV <rappytv@rappytv.com>
2024-03-31 23:35:36 +0200

    Don't log /ping route

--- src/middleware/AccessLog.ts
+++ src/middleware/AccessLog.ts
@@ -7,5 +7,5 @@ export default function access({ request: { headers, method }, path }: Context) 
     const version = headers.get(`x-addon-version`) ? `Addon v${headers.get(`x-addon-version`)}` : `API`;
     const time = moment(new Date()).format(logTimeFormat);
 
-    Logger.debug(`[${time}] ${method} ${path} [${version}] [${!!headers.get(`authorization`) ? `` : `NO `}AUTH]`)
+    if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${version}] [${!!headers.get(`authorization`) ? `` : `NO `}AUTH]`)
 }
