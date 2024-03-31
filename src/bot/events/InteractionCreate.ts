5d5fb9fdf99dc242a47d6963cdcf2c0b37877a51
RappyTV <rappytv@rappytv.com>
2024-03-31 18:03:15 +0200

    Implement bot events

---
+++ src/bot/events/InteractionCreate.ts
@@ -0,0 +1,46 @@
+import { CommandInteractionOptionResolver, EmbedBuilder, GuildMember, Interaction } from "discord.js";
+import Event from "../structs/Event";
+import * as bot from "../bot";
+
+export default class InteractionCreate extends Event {
+    constructor() {
+        super("interactionCreate", false);
+    }
+
+    async fire(interaction: Interaction) {
+        if(interaction.isChatInputCommand()) {
+            const { member, user, commandName, options } = interaction;
+            const cmd = bot.commands.get(commandName);
+
+            if(!cmd) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setDescription(`❌ Unknown command!`)], ephemeral: true });
+
+            try {
+                cmd.execute(interaction, options as CommandInteractionOptionResolver, member as GuildMember, user);
+            } catch(err: any) {
+                if(!interaction.replied) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setTitle(`❌ An error ocurred!`).setDescription(err)], ephemeral: true });
+            }
+        } else if(interaction.isButton()) {
+            const { member, user, customId, message } = interaction;
+            const btn = bot.buttons.get(customId);
+
+            if(!btn) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setDescription(`❌ Unknown button!`)], ephemeral: true });
+
+            try {
+                btn.trigger(interaction, message, member as GuildMember, user);
+            } catch(err: any) {
+                if(!interaction.replied) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setTitle(`❌ An error ocurred!`).setDescription(err)], ephemeral: true });
+            }
+        } else if(interaction.isModalSubmit()) {
+            const { member, user, customId, fields, message } = interaction;
+            const modal = bot.modals.get(customId);
+
+            if(!modal) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setDescription(`❌ Unknown modal!`)], ephemeral: true });
+
+            try {
+                modal.submit(interaction, message!, fields, member as GuildMember, user);
+            } catch(err: any) {
+                if(!interaction.replied) return interaction.reply({ embeds: [new EmbedBuilder().setColor(bot.colors.error).setTitle(`❌ An error ocurred!`).setDescription(err)], ephemeral: true });
+            }
+        }
+    }
+}
