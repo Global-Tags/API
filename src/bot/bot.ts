8f0aae9f5cd791d0a23156427ce2bbfb8b3a37db
RappyTV <rappytv@rappytv.com>
2024-03-31 17:45:25 +0200

    Use newly created structs, type Collections with generics

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,6 +1,11 @@
 import { Client, Collection, IntentsBitField } from "discord.js";
 import { existsSync, readdirSync } from "fs";
 import { join } from "path";
+import Event from "./structs/Event";
+import Command from "./structs/Command";
+import Modal from "./structs/Modal";
+import Button from "./structs/Button";
+import * as config from "../../config.json";
 
 export const client = new Client({
     intents: [
@@ -20,38 +25,39 @@ export const colors = {
     error: 0xff0000
 };
 
-export const commands = new Collection();
-export const buttons = new Collection();
-export const menus = new Collection();
-export const modals = new Collection();
+export const commands = new Collection<string, Command>();
+export const buttons = new Collection<string, Button>();
+export const modals = new Collection<string, Modal>();
 
 (async () => {
     const eventDir = join(__dirname, `events`);
     const commandDir = join(__dirname, `commands`);
     const buttonDir = join(__dirname, `buttons`);
     const modalDir = join(__dirname, `modals`);
-    if(existsSync(eventDir)) readdirSync(eventDir).filter(file => file.endsWith(`.js`)).forEach(file => {
-        const event = require(join(eventDir, file));
+    if(existsSync(eventDir)) readdirSync(eventDir).filter(file => file.endsWith(`.ts`)).forEach(async file => {
+        const event = new (await import(join(eventDir, file))).default as Event;
 
         if(event.once) {
-            client.once(event.name, event.action);
+            client.once(event.name, event.fire);
         } else {
-            client.on(event.name, event.action);
+            client.on(event.name, event.fire);
         }
     });
-    if(existsSync(commandDir)) readdirSync(commandDir).filter(file => file.endsWith(`.js`)).forEach(file => {
-        const cmd = require(join(commandDir, file));
+    if(existsSync(commandDir)) readdirSync(commandDir).filter(file => file.endsWith(`.ts`)).forEach(async file => {
+        const cmd = new (await import(join(commandDir, file))).default as Command;
 
         commands.set(cmd.name, cmd);
     });
-    if(existsSync(buttonDir)) readdirSync(buttonDir).filter(file => file.endsWith(`.js`)).forEach(file => {
-        const btn = require(join(buttonDir, file));
+    if(existsSync(buttonDir)) readdirSync(buttonDir).filter(file => file.endsWith(`.ts`)).forEach(async file => {
+        const btn = new (await import(join(buttonDir, file))).default as Button;
 
         buttons.set(btn.id, btn);
     });
-    if(existsSync(modalDir)) readdirSync(modalDir).filter(file => file.endsWith(`.js`)).forEach(file => {
-        const modal = require(join(modalDir, file));
+    if(existsSync(modalDir)) readdirSync(modalDir).filter(file => file.endsWith(`.ts`)).forEach(async file => {
+        const modal = new (await import(join(modalDir, file))).default as Modal;
 
         modals.set(modal.id, modal);
     });
 })();
+
+client.login(config.bot.token);
