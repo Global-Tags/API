68e6b6c6810f72692c9446e02e756d3719fc01a7
RappyTV <rappytv@rappytv.com>
2024-03-31 22:00:50 +0200

    Add elysia-ip, check for ratelimit before handle

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,18 @@ import * as config from "../config.json";
 import { version } from "../package.json";
 import access from "./middleware/AccessLog";
 import checkDatabase from "./middleware/DatabaseChecker";
-
-// TODO: Implement ratelimiter
+import Ratelimiter from "./libs/Ratelimiter";
+import checkRatelimit from "./middleware/RatelimitChecker";
+import { ip } from "elysia-ip";
 
 // Elysia API
 export const api = new Elysia()
 .onRequest(checkDatabase)
 .onTransform(access)
+.onBeforeHandle(checkRatelimit)
 .get(`/`, () => ({ version }))
 .get(`/ping`, ({ error }: Context) => { return error(204, "") })
+.use(ip())
 .use(swagger({
     autoDarkMode: true,
     swaggerOptions: {
@@ -39,6 +42,7 @@ export const api = new Elysia()
 .use(getRouter(`/players/:uuid`, __dirname))
 .onStart(() => {
     Logger.info(`Elysia listening on port ${config.port}!`);
+    Ratelimiter.initialize();
 
     connect(config.srv);
 })
