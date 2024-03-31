b5204e125df99137331b8d0a3119f31722a81245
RappyTV <rappytv@rappytv.com>
2024-03-31 01:12:41 +0100

    Customize swagger

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,26 @@ connect(config.srv);
 export const api = new Elysia()
 .get(`/`, () => ({ version }))
 .get(`/ping`, ({ error }: Context) => { return error(204, "") })
-.use(swagger())
+.use(swagger({
+    autoDarkMode: true,
+    swaggerOptions: {
+        tryItOutEnabled: false
+    },
+    exclude: [
+        `/players/{uuid}/ban/`
+    ],
+    documentation: {
+        info: {
+            version,
+            title: `GlobalTags LabyMod Addon API`,
+            contact: {
+                name: `RappyTV`,
+                url: `https://www.rappytv.com`,
+                email: `contact@rappytv.com`
+            }
+        }
+    }
+}))
 .use(getRouter(`/players/:uuid`, __dirname))
 .onStart(() => Logger.info(`Elysia listening on port ${config.port}!`))
 .listen(config.port);
