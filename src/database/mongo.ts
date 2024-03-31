4e77bf1f791888c544f725a623450881aad846ee
RappyTV <rappytv@rappytv.com>
2024-03-31 18:38:19 +0200

    Launch bot after database connection

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import Logger from "../libs/Logger";
+import * as config from "../../config.json";
 
 export async function connect(srv: string) {
     return await mongoose.connect(srv)
     .then(() => {
         Logger.info("Connected to MongoDB!");
+        if(config.bot.enabled) import("../bot/bot");
     }).catch((err) => {
         Logger.error(`Failed to establish database connection! ${err}`);
         setTimeout(() => connect(srv), 5000);
