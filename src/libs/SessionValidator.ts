8df7f4d6d578acca5e7ed763dd6d5bb80f544356
RappyTV <rappytv@rappytv.com>
2024-04-10 10:53:05 +0200

    Move LabyConnect public key out of config

--- src/libs/SessionValidator.ts
+++ src/libs/SessionValidator.ts
@@ -1,5 +1,5 @@
 import { verify } from "jsonwebtoken";
-import { labyConnect } from "../../config.json";
+const publicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt3rCKqrQYcmSEE8zyQTA7flKIe1pr7GHY58lTF74Pw/ZZYzxmScYteXp8XBvrQfPj4U/v9Vum8IPg6GHOv1Gde3rY5ydfunEKi/w4ibVN5buPpndzcNaMoQvEJ/B5VLIzCvLc5HepFKbKFOGu8XoFz8NZY0lUfGLR0rcDsHWZLHPhqYsIsUd9snkWkHaIKD7l9xTd77PpLZiBwCPnVhh3invFY2OnCL6BfiJhhud/aDaAzFW981J9EhyACbuac2qu6Uz2bKX/7Af01gUs48MbKUx8YirBWLD7j/CJMWorTT467It4mAvDlw43s3Py9IvxCzEFnOIftIv+7wwv1RjVQIDAQAB\n-----END PUBLIC KEY-----";
 
 export function validJWTSession(token: string, uuid: string, equal: boolean): boolean {
     const tokenUuid = getUuidByJWT(token);
@@ -14,7 +14,7 @@ type LabyPayload = {
 
 export function getUuidByJWT(token: string): string | null {
     try {
-        const payload: LabyPayload = verify(token, labyConnect) as LabyPayload;
+        const payload: LabyPayload = verify(token, publicKey) as LabyPayload;
         if(payload.iss != `LabyConnect`) return null;
 
         return payload.uuid?.replaceAll(`-`, ``);
