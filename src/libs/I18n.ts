fe4e315b8ab89612cc08ecb74dfc15b769208a94
RappyTV <rappytv@rappytv.com>
2024-04-16 18:13:04 +0200

    Implement function that loads languages

--- src/libs/I18n.ts
+++ src/libs/I18n.ts
@@ -1,8 +1,23 @@
 import * as english from "../../locales/en-US.json";
+import { existsSync, readdirSync } from "fs";
+import { join } from "path";
+import Logger from "./Logger";
 
 export type Language = typeof english;
 
 const languages = new Map<string, Language>();
+
+export async function load() {
+    languages.clear();
+    const localeDir = join(__dirname, '..', '..', 'locales');
+    if(!existsSync(localeDir)) return Logger.error(`Locales directory not found!`);
+    for(const file of readdirSync(localeDir).filter((file) => file.endsWith(`.json`))) {
+        const id = file.replace(`.json`, '');
+        const locales = await import(join(localeDir, file)) as Language;
+        languages.set(id, locales);
+        Logger.debug(`Loaded language ${id}`);
+    }
+}
 
 export function getLocales(language: string): Language {
     if(languages.has(language)) return languages.get(language)!;
