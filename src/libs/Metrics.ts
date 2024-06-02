21f52162897a5eb475f0f1f7dbc5280a0bb73ee6
RappyTV <rappytv@rappytv.com>
2024-06-02 15:39:11 +0200

    Save downloads and rating in metrics

--- src/libs/Metrics.ts
+++ src/libs/Metrics.ts
@@ -1,6 +1,37 @@
 import { CronJob } from "cron";
 import metrics from "../database/schemas/metrics";
 import players from "../database/schemas/players";
+import Logger from "./Logger";
+import axios from "axios";
+
+type Addon = {
+    id: number,
+    namespace: string,
+    name: string,
+    featured: boolean,
+    verified: boolean,
+    organization: number,
+    author: string,
+    downloads: number,
+    download_string: string,
+    short_description: string,
+    rating: {
+        count: number,
+        rating: number
+    },
+    changelog: string,
+    required_labymod_build: number,
+    releases: number,
+    last_update: number,
+    licence: string,
+    version_string: string,
+    meta: [],
+    dependencies: [{ namespace: string, optional: boolean }],
+    permissions: [string],
+    source_url: string,
+    brand_images: [{ type: string, hash: string }],
+    tags: [number]
+}
 
 export function initializeMetrics() {
     new CronJob(`0 0 * * *`, saveMetrics, null, true);
@@ -19,13 +50,26 @@ async function saveMetrics() {
         object[icon.toLowerCase()] = users.filter((user) => user.icon.toUpperCase() == icon.toUpperCase()).length;
         return object;
     }, {});
+    const addon = await fetchAddon('globaltags');
     
     new metrics({
         players: users.length,
         tags,
         admins,
         bans,
+        downloads: addon?.downloads ?? -1,
+        rating: addon?.rating.rating ?? -1,
         positions,
         icons
     }).save();
+}
+
+async function fetchAddon(namespace: string): Promise<Addon | null> {
+    try {
+        const data = await axios.get(`https://flintmc.net/api/client-store/get-modification/${namespace}`, { headers: { 'Accept-Encoding': 'gzip' } });
+        return data.data as Addon;
+    } catch(error) {
+        Logger.error(`Error while trying to fetch addon "${namespace}": ${error}`);
+        return null;
+    }
 }
