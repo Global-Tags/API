8ff7812f02603d1835ec4a225bb0c32ea9361524
RappyTV <rappytv@rappytv.com>
2024-06-29 18:20:20 +0200

    Add more staff checks on admin actions, log linked uuids

--- src/bot/buttons/FinishAction.ts
+++ src/bot/buttons/FinishAction.ts
@@ -1,18 +1,23 @@
 import { ButtonInteraction, CacheType, Message, GuildMember, User, ActionRowBuilder, EmbedBuilder, ButtonBuilder, ActionRow } from "discord.js";
 import Button from "../structs/Button";
 import { colors } from "../bot";
+import players from "../../database/schemas/players";
 
 export default class FinishAction extends Button {
     constructor() {
         super("finishAction");
     }
 
-    public trigger(interaction: ButtonInteraction<CacheType>, message: Message<boolean>, member: GuildMember, user: User) {
+    async trigger(interaction: ButtonInteraction<CacheType>, message: Message<boolean>, member: GuildMember, user: User) {
+        const staff = await players.findOne({ "connections.discord.id": user.id });
+        if(!staff) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You need to link your Minecraft account with \`/link\`!`)], ephemeral: true });
+        if(!staff.admin) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You're not allowed to perform this action!`)], ephemeral: true });
+
         const row = ActionRowBuilder.from(message.components[0] as any) as ActionRowBuilder<ButtonBuilder>;
         row.components.forEach(component => component.setDisabled(true));
 
         const embed = EmbedBuilder.from(message.embeds[0]);
-        embed.setFooter({ text: `Processed by ${user.username}`, iconURL: user.displayAvatarURL() });
+        embed.setFooter({ text: `Processed by ${user.username}`, iconURL: `https://laby.net/texture/profile/head/${staff.uuid.replaceAll(`-`, ``)}.png?size=1024&overlay` });
 
         message.edit({ embeds: [embed], components: [row] });
         interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription(`✅ Action completed!`)], ephemeral: true });
--- src/bot/modals/SetTag.ts
+++ src/bot/modals/SetTag.ts
@@ -10,6 +10,10 @@ export default class SetTag extends Modal {
     }
 
     async submit(interaction: ModalSubmitInteraction<CacheType>, message: Message<boolean>, fields: ModalSubmitFields, member: GuildMember, user: User) {
+        const staff = await players.findOne({ "connections.discord.id": user.id });
+        if(!staff) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You need to link your Minecraft account with \`/link\`!`)], ephemeral: true });
+        if(!staff.admin) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You're not allowed to perform this action!`)], ephemeral: true });
+
         const player = await players.findOne({ uuid: message.embeds[0].fields[0].value.replaceAll(`\``, ``) });
         if(!player) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ Player not found!`)], ephemeral: true });
         const tag = fields.getTextInputValue('tag');
@@ -18,7 +22,7 @@ export default class SetTag extends Modal {
             type: NotificationType.ModLog,
             logType: ModLogType.ChangeTag,
             uuid: player.uuid,
-            staff: user.id,
+            staff: staff.uuid,
             oldTag: player.tag || 'None',
             newTag: tag,
             discord: true
