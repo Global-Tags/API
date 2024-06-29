9d5bed9cc0866969fa294e2fdeeb0b8f31ab58e6
RappyTV <rappytv@rappytv.com>
2024-06-29 18:10:36 +0200

    Add /link to finish account connection and /unlink to remove connection

---
+++ src/bot/commands/Unlink.ts
@@ -0,0 +1,37 @@
+import { ApplicationCommandOptionType, CacheType, CommandInteraction, CommandInteractionOptionResolver, EmbedBuilder, GuildMember, User } from "discord.js";
+import Command from "../structs/Command";
+import players from "../../database/schemas/players";
+import { colors } from "../bot";
+import { bot } from "../../../config.json";
+import { NotificationType, sendMessage } from "../../libs/DiscordNotifier";
+
+export default class PlayerInfo extends Command {
+    constructor() {
+        super(
+            "unlink",
+            "Unlink your Discord account from your Minecraft account.",
+            []
+        );
+    }
+
+    async execute(interaction: CommandInteraction<CacheType>, options: CommandInteractionOptionResolver<CacheType>, member: GuildMember, user: User) {
+        await interaction.deferReply({ ephemeral: true });
+        if(!bot.connection.active) return interaction.editReply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ Account linking is deactivated!`)] });
+
+        const player = await players.findOne({ "connections.discord.id": user.id });
+        if(!player) return interaction.editReply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ Your Discord account is not linked to any Minecraft account!`)] });
+
+        player.connections!.discord!.id = null;
+        player.connections!.discord!.code = null;
+        player.save();
+
+        sendMessage({
+            type: NotificationType.DiscordLink,
+            connected: false,
+            userId: user.id,
+            uuid: player.uuid
+        });
+
+        interaction.editReply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription(`✅ Your account was successfully unlinked!`)] });
+    }
+}
