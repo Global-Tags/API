48cd641932137e466d01f2dd39666e348737d53e
RappyTV <rappytv@rappytv.com>
2024-06-29 18:52:01 +0200

    Add routes to link and unlink a discord account

---
+++ src/routes/connections.ts
@@ -0,0 +1,77 @@
+import Elysia, { t } from "elysia";
+import { getJWTSession } from "../libs/SessionValidator";
+import players from "../database/schemas/players";
+import fetchI18n from "../middleware/FetchI18n";
+import { bot } from "../../config.json";
+
+export default new Elysia({
+    prefix: "/connections"
+}).use(fetchI18n).post(`/discord`, async ({ error, params, headers, body, i18n }) => { // Get a linking code
+    if(!bot.connection.active) return error(409, { error: i18n('connections.discord.disabled') });
+    const uuid = params.uuid.replaceAll(`-`, ``);
+    const { authorization } = headers;
+    const session = await getJWTSession(authorization, uuid);
+    if(!session.equal) return error(403, { error: i18n(`error.notAllowed`) });
+
+    const player = await players.findOne({ uuid });
+    if(!player) return error(404, { error: i18n(`error.noTag`) });
+    if(player.isBanned()) return error(403, { error: i18n(`error.banned`) });
+    if(player.connections!.discord!.id) return error(400, { error: i18n(`connections.discord.alreadyConnected`) });
+
+    const code = Date.now().toString(36);
+    player.connections!.discord!.code = code;
+    await player.save();
+
+    return { code };
+}, {
+    detail: {
+        tags: ['Connections'],
+        description: `Receive a code to link your Discord account with '/link' on the discord server`
+    },
+    response: {
+        200: t.Object({ code: t.String() }, { description: `You received a linking code.` }),
+        400: t.Object({ error: t.String() }, { description: `You already have a Discord account connected.` }),
+        403: t.Object({ error: t.String() }, { description: `You're banned.` }),
+        404: t.Object({ error: t.String() }, { description: `You don't have a GlobalTags account.` }),
+        409: t.Object({ error: t.String() }, { description: `Account linking is deactivated.` }),
+        422: t.Object({ error: t.String() }, { description: `You're lacking the validation requirements.` }),
+        429: t.Object({ error: t.String() }, { description: `You're ratelimited.` }),
+        503: t.Object({ error: t.String() }, { description: `Database is not reachable.` })
+    },
+    params: t.Object({ uuid: t.String({ description: `Your UUID` }) }),
+    headers: t.Object({ authorization: t.String({ error: `error.notAllowed`, description: `Your LabyConnect JWT` }) }, { error: `error.notAllowed` })
+}).delete(`/discord`, async ({ error, params, headers, body, i18n }) => { // Change icon
+    if(!bot.connection.active) return error(409, { error: i18n('connections.discord.disabled') });
+    const uuid = params.uuid.replaceAll(`-`, ``);
+    const { authorization } = headers;
+    const session = await getJWTSession(authorization, uuid);
+    if(!session.equal) return error(403, { error: i18n(`error.notAllowed`) });
+
+    const player = await players.findOne({ uuid });
+    if(!player) return error(404, { error: i18n(`error.noTag`) });
+    if(player.isBanned()) return error(403, { error: i18n(`error.banned`) });
+    if(!player.connections!.discord!.id) return error(400, { error: i18n(`connections.discord.notConnected`) });
+
+    player.connections!.discord!.id = null;
+    player.connections!.discord!.code = null;
+    await player.save();
+
+    return { message: i18n('connections.discord.unlinked') };
+}, {
+    detail: {
+        tags: ['Connections'],
+        description: `Unlink your connected discord account.`
+    },
+    response: {
+        200: t.Object({ message: t.String() }, { description: `Your account was successfully unlinked.` }),
+        400: t.Object({ error: t.String() }, { description: `You don't have a Discord account connected.` }),
+        403: t.Object({ error: t.String() }, { description: `You're banned.` }),
+        404: t.Object({ error: t.String() }, { description: `You don't have a GlobalTags account.` }),
+        409: t.Object({ error: t.String() }, { description: `Account linking is deactivated.` }),
+        422: t.Object({ error: t.String() }, { description: `You're lacking the validation requirements.` }),
+        429: t.Object({ error: t.String() }, { description: `You're ratelimited.` }),
+        503: t.Object({ error: t.String() }, { description: `Database is not reachable.` })
+    },
+    params: t.Object({ uuid: t.String({ description: `Your UUID` }) }),
+    headers: t.Object({ authorization: t.String({ error: `error.notAllowed`, description: `Your LabyConnect JWT` }) }, { error: `error.notAllowed` })
+});
