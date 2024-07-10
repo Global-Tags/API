c84a851a31cddeb7f256c0ea6c6983bbf399040c
RappyTV <rappytv@rappytv.com>
2024-07-11 00:43:32 +0200

    Implement editRoles select menu

---
+++ src/bot/menus/EditRoles.ts
@@ -0,0 +1,48 @@
+import { StringSelectMenuInteraction, Message, GuildMember, User, EmbedBuilder } from "discord.js";
+import SelectMenu from "../structs/SelectMenu";
+import players from "../../database/schemas/players";
+import { colors } from "../bot";
+import { ModLogType, NotificationType, sendMessage } from "../../libs/DiscordNotifier";
+
+export default class EditRoles extends SelectMenu {
+    constructor() {
+        super('editRoles');
+    }
+
+    async selection(interaction: StringSelectMenuInteraction, message: Message, values: string[], member: GuildMember, user: User) {
+        const staff = await players.findOne({ "connections.discord.id": user.id });
+        if(!staff) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You need to link your Minecraft account with \`/link\`!`)], ephemeral: true });
+        if(!staff.isAdmin()) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ You're not allowed to perform this action!`)], ephemeral: true });
+
+        const player = await players.findOne({ uuid: message.embeds[0].fields[0].value.replaceAll(`\``, ``) });
+        if(!player) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ Player not found!`)], ephemeral: true });
+        if(player.isBanned()) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription(`❌ This player is already banned!`)], ephemeral: true });
+
+        const added: string[] = [];
+        const removed: string[] = [];
+
+        for(const role of player.roles) {
+            if(values.includes(role)) added.push(role);
+            else removed.push(role);
+        }
+
+        player.roles.length = 0;
+        for(const role of values) {
+            player.roles.push(role);
+        }
+
+        sendMessage({
+            type: NotificationType.ModLog,
+            logType: ModLogType.Ban,
+            uuid: player.uuid,
+            staff: staff.uuid,
+            roles: {
+                added,
+                removed
+            },
+            discord: true
+        });
+
+        interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription(`✅ The players roles were successfully updated!`)], ephemeral: true });
+    }
+}
