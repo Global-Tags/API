3e6d1e78e1749ea2203809d1f4ff047c57a15963
RappyTV <rappytv@rappytv.com>
2024-07-12 22:34:45 +0200

    Don't cancel request when auth provider is not found

--- src/middleware/GetAuthProvider.ts
+++ src/middleware/GetAuthProvider.ts
@@ -1,14 +1,12 @@
 import AuthProvider from "../auth/AuthProvider";
 import Elysia from "elysia";
-import { getI18nFunctionByLanguage } from "./FetchI18n";
 
 export default function getAuthProvider(app: Elysia) {
-    return app.derive({ as: 'global' }, ({ headers, error }) => {
-        const i18n = getI18nFunctionByLanguage(headers['x-minecraft-language']);
+    return app.derive({ as: 'global' }, ({ headers }) => {
         const header = headers['authorization'];
         if(!header) return { provider: null };
         const provider = AuthProvider.getProvider(header);
-        if(!provider) return error(401, { error: i18n('error.malformedAuthHeader') });
+        if(!provider) return { provder: null };
         return { provider };
     });
 }
