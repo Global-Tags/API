4f0448f9315a1d7a444458d4a4aad7e52ebdfb90
RappyTV <rappytv@rappytv.com>
2024-07-12 22:25:43 +0200

    Add LabyConnect and Minecraft Yggdrasil auth providers

---
+++ src/auth/providers/YggdrasilProvider.ts
@@ -0,0 +1,24 @@
+import AuthProvider, { SessionData } from "../AuthProvider";
+import axios from "axios";
+
+export default class LabyConnectProvider extends AuthProvider {
+    constructor() {
+        super('Minecraft');
+    }
+
+    async getUUID(token: string): Promise<string | null> {
+        try {
+            // TODO: Fix request
+            const response = await axios({
+                method: 'GET',
+                url: '...',
+                data: {
+
+                }
+            });
+            return response.data.uuid as string;
+        } catch(err) {
+            return null;
+        }
+    }
+}
