a19ab7b86c5a39cc5a23e6e469ade5b1109ba967
RappyTV <rappytv@rappytv.com>
2024-07-13 21:33:55 +0200

    Include AuthProvider id in access log

--- src/middleware/AccessLog.ts
+++ src/middleware/AccessLog.ts
@@ -2,10 +2,12 @@ import { Context } from "elysia";
 import Logger from "../libs/Logger";
 import moment = require("moment");
 import { logTimeFormat } from "../../config.json";
+import AuthProvider from "../auth/AuthProvider";
 
 export default function access({ request: { headers, method }, path }: Context) {
-    const version = headers.get(`x-addon-version`) ? `Addon v${headers.get(`x-addon-version`)}` : `API`;
+    const version = headers.get('x-addon-version') ? `Addon v${headers.get(`x-addon-version`)}` : 'API';
+    const auth = AuthProvider.getProvider(headers.get('authorization') || '')?.id || 'None';
     const time = moment(new Date()).format(logTimeFormat);
 
-    if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${version}] [${!!headers.get(`authorization`) ? `` : `NO `}AUTH]`)
+    if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${version}] [${auth}]`);
 }
