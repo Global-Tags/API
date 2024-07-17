4255c8776c227c20180c79fe392281acb01fd3f4
RappyTV <rappytv@rappytv.com>
2024-07-17 23:31:49 +0200

    Implement new role and permission methods on player schema

--- src/database/schemas/players.ts
+++ src/database/schemas/players.ts
@@ -1,10 +1,22 @@
 import { Schema, model } from "mongoose";
+import { bot, roles } from "../../../config.json";
+import { client } from "../../bot/bot";
 
-export enum Role {
-    ADMIN,
-    DEVELOPER,
-    MODERATOR,
-    SUPPORTER
+export type Role = {
+    name: string,
+    permissions: {
+        ManageAdmins: boolean,
+        ManageBans: boolean,
+        ManageTags: boolean,
+        ManageWatchlist: boolean
+    }
+}
+
+export enum Permission {
+    ManageAdmins,
+    ManageBans,
+    ManageTags,
+    ManageWatchlist
 }
 
 export interface IPlayer {
@@ -23,7 +35,9 @@ export interface IPlayer {
     connections: {
         discord?: { id?: string | null, code?: string | null }
     },
-    isAdmin(): boolean,
+    getRoles(): Promise<string[]>,
+    getPermissions(): Promise<{ [key: string]: boolean }>,
+    hasPermission(permission: Permission): Promise<boolean>,
     isBanned(): boolean,
     banPlayer(reason: string, appealable?: boolean): void,
     unban(): void
@@ -123,8 +137,41 @@ const schema = new Schema<IPlayer>({
     }
 }, {
     methods: {
-        isAdmin(): boolean {
-            return this.roles.includes(Role[Role.ADMIN]);
+        async getRoles() {
+            if(!bot.synced_roles.enabled) return roles.filter((role) => this.roles.some((name) => name.toUpperCase() == role.name.toUpperCase())).map((role) => role.name);
+            if(!this.connections?.discord?.id) return [];
+            const guild = await client.guilds.fetch(bot.synced_roles.guild);
+            if(!guild) return [];
+            const member = await guild.members.fetch(this.connections.discord.id);
+            if(!member) return [];
+            return Object
+                .keys(bot.synced_roles.roles)
+                .filter((key) => {
+                    const role = roles.find((role) => role.name.toUpperCase() == key.toUpperCase());
+                    if(!role) return false;
+                    const roleIds = bot.synced_roles.roles[role.name as keyof typeof bot.synced_roles.roles];
+                    if(!roleIds) return false;
+                    return roleIds.some((id) => member.roles.cache.has(id));
+                })
+                .map((role) => role);
+        },
+
+        async getPermissions() {
+            const localRoles = await this.getRoles();
+            const permissions: { [key: string]: boolean } = {};
+            for(const permission of Object.keys(Permission).filter(key => isNaN(Number(key)))) {
+                permissions[permission] = localRoles.some((key) => {
+                    const role = roles.find((r) => r.name == key)!;
+                    if(!role) return false;
+                    return role.permissions[permission as keyof typeof role.permissions];
+                });
+            }
+            return permissions;
+        },
+
+        async hasPermission(permission: Permission) {
+            const permissions = await this.getPermissions();
+            return permissions[Permission[permission]];
         },
 
         isBanned(): boolean {
