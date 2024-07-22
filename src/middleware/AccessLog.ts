75c3f66c875c10d85e8d370d3ac7e5bd0974afbe
RappyTV <rappytv@rappytv.com>
2024-07-22 20:23:16 +0200

    Improve formatting of auth header in access log

--- src/middleware/AccessLog.ts
+++ src/middleware/AccessLog.ts
@@ -5,9 +5,9 @@ import { logTimeFormat } from "../../config.json";
 import AuthProvider from "../auth/AuthProvider";
 
 export default function access({ request: { headers, method }, path }: Context) {
-    const authorization = headers.get('authorization');
+    const authorization = headers.get('authorization') || '';
     const version = headers.get('x-addon-version') ? `Addon v${headers.get(`x-addon-version`)}` : 'API';
-    const auth = AuthProvider.getProvider(authorization || '')?.id || `None: ${authorization}`;
+    const auth = AuthProvider.getProvider(authorization)?.id || `None${authorization.trim().length > 0 ? `: ${authorization}` : ''}`;
     const time = moment(new Date()).format(logTimeFormat);
 
     if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${version}] [${auth}]`);
