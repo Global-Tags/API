b19b6b95b4e249611f8d367533db47782ed83893
RappyTV <rappytv@rappytv.com>
2024-07-25 16:15:29 +0200

    Change access log with new agent header

--- src/middleware/AccessLog.ts
+++ src/middleware/AccessLog.ts
@@ -6,9 +6,9 @@ import AuthProvider from "../auth/AuthProvider";
 
 export default function access({ request: { headers, method }, path }: Context) {
     const authorization = headers.get('authorization') || '';
-    const version = headers.get('x-addon-version') ? `Addon v${headers.get(`x-addon-version`)}` : 'API';
+    const agent = headers.get('x-agent') ? headers.get('x-agent') : 'API';
     const auth = AuthProvider.getProvider(authorization)?.id || `None${authorization.trim().length > 0 ? `: ${authorization}` : ''}`;
     const time = moment(new Date()).format(logTimeFormat);
 
-    if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${version}] [${auth}]`);
+    if(path != `/ping`) Logger.debug(`[${time}] ${method} ${path} [${agent}] [${auth}]`);
 }
