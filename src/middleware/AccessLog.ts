185d3310e35bb9dce038a238afce339dea96121a
RappyTV <rappytv@rappytv.com>
2024-12-05 15:44:20 +0100

    Remove unnecessary logTimeFormat config option

--- src/middleware/AccessLog.ts
+++ src/middleware/AccessLog.ts
@@ -1,7 +1,6 @@
 import { Context } from "elysia";
 import Logger from "../libs/Logger";
 import moment from "moment";
-import { logTimeFormat } from "../../config.json";
 import AuthProvider from "../auth/AuthProvider";
 import { recordRequest } from "../libs/Metrics";
 
@@ -9,9 +8,9 @@ export default function access({ request: { headers, method }, path }: Context) 
     const authorization = headers.get('authorization') || '';
     const agent = headers.get('x-agent') ? headers.get('x-agent') : 'API';
     const auth = AuthProvider.getProvider(authorization)?.id || `None${authorization.trim().length > 0 ? `: ${authorization}` : ''}`;
-    const time = moment(new Date()).format(logTimeFormat);
+    const time = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
 
-    if(path != `/ping`) {
+    if(path != '/ping') {
         recordRequest();
         Logger.debug(`[${time}] ${method} ${path} [${agent}] [${auth}]`);
     }
