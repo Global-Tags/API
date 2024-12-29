6c3b136d8de3a7a72ab392de319133457386158b
RappyTV <rappytv@rappytv.com>
2024-12-29 15:03:41 +0100

    Export variable containing all permission enum values

--- src/types/Permission.ts
+++ src/types/Permission.ts
@@ -10,3 +10,5 @@ export enum Permission {
     ManageWatchlist,
     ReportImmunity
 }
+
+export const permissions = Object.keys(Permission).filter(key => isNaN(Number(key))).map((permission) => Permission[permission as keyof typeof Permission]);
