899a3bdd51b0fb3918073a87c8985413e51dd2ef
RappyTV <rappytv@rappytv.com>
2025-02-14 17:52:21 +0100

    Remove synced roles when unlinking discord

--- src/libs/events.ts
+++ src/libs/events.ts
@@ -4,6 +4,7 @@ import { getCachedRoles, synchronizeRoles } from "../database/schemas/roles";
 import { config } from "./config";
 import { sendDiscordLinkMessage } from "./discord-notifier";
 import { Profile } from "./game-profiles";
+import Logger from "./Logger";
 
 export async function onDiscordLink(player: Profile, userId: string) {
     sendDiscordLinkMessage(
@@ -40,16 +41,32 @@ export async function onDiscordLink(player: Profile, userId: string) {
     member.roles.add(config.discordBot.notifications.accountConnections.role);
 }
 
-export async function onDiscordUnlink(player: Profile, userId: string) {
-    sendDiscordLinkMessage(
-        player,
-        userId,
-        false
-    );
-
-    const guild = await fetchGuild();
-    if(!guild) return;
-    const member = await guild.members.fetch(userId);
-    if(!member) return;
-    member.roles.remove(config.discordBot.notifications.accountConnections.role);
+export function onDiscordUnlink(player: Profile, userId: string): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+        sendDiscordLinkMessage(
+            player,
+            userId,
+            false
+        );
+    
+        const guild = await fetchGuild();
+        if(!guild) return;
+        const member = await guild.members.fetch(userId);
+        if(!member) return;
+        member.roles.remove(config.discordBot.notifications.accountConnections.role);
+        const playerData = await players.findOne({ 'connections.discord.id': userId });
+        if(playerData) {
+            for(const role of playerData.getRoles()) {
+                const syncedRoles = role.role.getSyncedRoles();
+                if(syncedRoles.length == 0) continue;
+                for(const syncedRole of syncedRoles) {
+                    if(!member.roles.cache.has(syncedRole)) continue;
+                    member.roles.remove(syncedRole)
+                        .then(() => Logger.debug(`Removed synced role "${syncedRole}" (${role.role.name}) from member "${member.id}".`))
+                        .catch((error) => Logger.error(`Failed to remove synced role "${syncedRole}" (${role.role.name}) from member "${member.id}": ${error}`));
+                }
+            }
+        }
+        resolve();
+    });
 }
