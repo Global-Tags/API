802135aecae3b0e53a8a7cd7bf6c89c86fd8338b
RappyTV <rappytv@rappytv.com>
2025-02-15 14:25:38 +0100

    Add route to retrieve current watchlist status

--- src/routes/players/[uuid]/watchlist.ts
+++ src/routes/players/[uuid]/watchlist.ts
@@ -5,7 +5,30 @@ import { ModLogType, sendModLogMessage } from "../../../libs/discord-notifier";
 import { GameProfile, stripUUID } from "../../../libs/game-profiles";
 import { Permission } from "../../../types/Permission";
 
-export default (app: ElysiaApp) => app.patch('/', async ({ session, body: { watched }, params, i18n, error }) => { // Watch player
+export default (app: ElysiaApp) => app.get('/', async ({ session, params, i18n, error }) => { // Watch player
+    if(!session?.hasPermission(Permission.ManageWatchlist)) return error(403, { error: i18n('error.notAllowed') });
+    const uuid = stripUUID(params.uuid);
+    
+    const player = await players.findOne({ uuid });
+    if(!player) return error(404, { error: i18n('error.playerNotFound') });
+
+    return { watched: player.watchlist };
+}, {
+    detail: {
+        tags: ['Admin'],
+        description: 'Returns the player\'s watchlist status'
+    },
+    response: {
+        200: t.Object({ watched: t.Boolean() }, { description: 'The player\'s watchlist status' }),
+        403: t.Object({ error: t.String() }, { description: 'You\'re not allowed to manage the watchlist' }),
+        404: t.Object({ error: t.String() }, { description: 'The player was not found' }),
+        422: t.Object({ error: t.String() }, { description: 'You\'re lacking the validation requirements' }),
+        429: t.Object({ error: t.String() }, { description: 'You\'re ratelimited' }),
+        503: t.Object({ error: t.String() }, { description: 'The database is not reachable' })
+    },
+    params: t.Object({ uuid: t.String({ description: 'The player\'s UUID' }) }),
+    headers: t.Object({ authorization: t.String({ error: 'error.notAllowed', description: 'Your authentication token' }) }, { error: 'error.notAllowed' })
+}).patch('/', async ({ session, body: { watched }, params, i18n, error }) => { // Watch player
     if(!session?.hasPermission(Permission.ManageWatchlist)) return error(403, { error: i18n('error.notAllowed') });
     const uuid = stripUUID(params.uuid);
     
