b068cfe2acf09c0416b040ddd07cc826002b3132
RappyTV <rappytv@rappytv.com>
2025-04-18 21:55:14 +0200

    Add button to show tag and icon clears

--- src/bot/buttons/ModerateAccount.ts
+++ src/bot/buttons/ModerateAccount.ts
@@ -5,6 +5,7 @@ import { colors } from "../bot";
 import { Permission } from "../../types/Permission";
 import { GameProfile, stripUUID } from "../../libs/game-profiles";
 import { formatTimestamp } from "../../libs/discord-notifier";
+import { InfoEntry } from "./Actions";
 
 export default class ModerateAccountButton extends Button {
     constructor() {
@@ -15,46 +16,53 @@ export default class ModerateAccountButton extends Button {
     }
 
     async trigger(interaction: ButtonInteraction, message: Message, member: GuildMember, player: Player) {
-        if(!player.hasPermission(Permission.ManageBans) && !player.hasPermission(Permission.ManageNotes) && !player.hasPermission(Permission.ManageReports) && !player.hasPermission(Permission.ManageWatchlist)) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ You\'re not allowed to perform this action!')], flags: [MessageFlags.Ephemeral] });
+        if(![
+            Permission.ManageBans,
+            Permission.ManageNotes,
+            Permission.ManageReports,
+            Permission.ManageWatchlist,
+            Permission.ManageTags
+        ].some((permission) => player.hasPermission(permission))) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ You\'re not allowed to perform this action!')], flags: [MessageFlags.Ephemeral] });
 
         const target = await players.findOne({ uuid: stripUUID(message.embeds[0].author!.name) });
         if(!target) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ Player not found!')], flags: [MessageFlags.Ephemeral] });
 
-        const banInfo = target.isBanned() ? await (async () => {
-            const ban = target.bans[0];
-            return `\n> Ban ID: \`${ban.id}\`\n> Ban reason: \`${ban.reason}\`\n> Banned by: [\`${(await GameProfile.getProfileByUUID(ban.staff)).getUsernameOrUUID()}\`](https://laby.net/@${player.uuid})\n> Banned since: ${formatTimestamp(ban.banned_at)}\n> Expiration date: ${ban.expires_at ? `${formatTimestamp(ban.expires_at)}` : '`-`'}\n> Is ban appealable: \`${ban.appealable ? '✅' : '❌'}\`\n> Was ban appealed: \`${ban.appealed ? '✅' : '❌'}\``;
-        })() : '';
+        const info: InfoEntry[] = [];
+
+        if(player.hasPermission(Permission.ManageWatchlist)) {
+            info.push({ name: 'Watchlist', value: `\`${target.watchlist ? '✅' : '❌'}\`` });
+        }
+        if(player.hasPermission(Permission.ManageBans)) {
+            info.push({ name: 'Active ban', value: `\`${target.isBanned() ? '✅' : '❌'}\`` });
+            if(target.isBanned()) {
+                const ban = target.bans[0];
+                info.push({ name: 'Ban ID', value: `\`${ban.id}\`` });
+                info.push({ name: 'Ban reason', value: `\`${ban.reason}\`` });
+                info.push({ name: 'Banned by', value: `[${(await GameProfile.getProfileByUUID(ban.staff)).getUsernameOrUUID()}](https://laby.net/@${player.uuid})` });
+                info.push({ name: 'Banned since', value: `${formatTimestamp(ban.banned_at)}` });
+                info.push({ name: 'Expiration date', value: `${ban.expires_at ? `${formatTimestamp(ban.expires_at)}` : '`-`'}` });
+                info.push({ name: 'Is ban appealable', value: `\`${ban.appealable ? '✅' : '❌'}\`` });
+                info.push({ name: 'Was ban appealed', value: `\`${ban.appealed ? '✅' : '❌'}\`` });
+            }
+        }
 
         const embed = EmbedBuilder.from(message.embeds[0])
-            .setTitle('Manage account')
-            .setDescription(`> Is on Watchlist: \`${target.watchlist ? '✅' : '❌'}\`\n> Active ban: \`${target.isBanned() ? '✅' : '❌'}\`${banInfo}`);
+            .setTitle('Moderate account')
+            .setDescription(info.length > 0 ? info.map(({ name, value }) => `> ${name}: ${value}`).join('\n') : '*No information available*');
 
         const rows = [
             new ActionRowBuilder<ButtonBuilder>()
                 .addComponents(
                     new ButtonBuilder()
-                        .setLabel('Ban')
-                        .setCustomId('ban')
-                        .setStyle(ButtonStyle.Danger)
-                        .setDisabled(!player.hasPermission(Permission.ManageBans)),
-                    new ButtonBuilder()
-                        .setLabel('Unban')
-                        .setCustomId('unban')
-                        .setStyle(ButtonStyle.Success)
-                        .setDisabled(!player.hasPermission(Permission.ManageBans))
-                ),
-            new ActionRowBuilder<ButtonBuilder>()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setLabel('Watch')
-                        .setCustomId('watch')
-                        .setStyle(ButtonStyle.Danger)
+                        .setLabel('Manage Watchlist')
+                        .setCustomId('manageWatchlist')
+                        .setStyle(ButtonStyle.Primary)
                         .setDisabled(!player.hasPermission(Permission.ManageWatchlist)),
                     new ButtonBuilder()
-                        .setLabel('Unwatch')
-                        .setCustomId('unwatch')
-                        .setStyle(ButtonStyle.Success)
-                        .setDisabled(!player.hasPermission(Permission.ManageWatchlist))
+                        .setLabel('Manage Bans')
+                        .setCustomId('manageBans')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(!player.hasPermission(Permission.ManageBans))
                 ),
             new ActionRowBuilder<ButtonBuilder>()
                 .addComponents(
@@ -67,7 +75,12 @@ export default class ModerateAccountButton extends Button {
                         .setLabel(`Notes${target.notes.length > 0 ? ` (${target.notes.length})` : ''}`)
                         .setCustomId('notes')
                         .setStyle(ButtonStyle.Primary)
-                        .setDisabled(!player.hasPermission(Permission.ManageNotes))
+                        .setDisabled(!player.hasPermission(Permission.ManageNotes)),
+                    new ButtonBuilder()
+                        .setLabel(`Clears${target.clears.length > 0 ? ` (${target.clears.length})` : ''}`)
+                        .setCustomId('clears')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(!player.hasPermission(Permission.ManageTags))
                 )
         ];
 
