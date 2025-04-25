ab41a916b5a5e12e0a9520809611421a4f88bbe4
RappyTV <rappytv@rappytv.com>
2025-04-25 23:35:21 +0200

    Add route to upload custom icons via formdata

--- src/routes/players/[uuid]/icon.ts
+++ src/routes/players/[uuid]/icon.ts
@@ -10,6 +10,9 @@ import { ElysiaApp } from "../../..";
 import { ModLogType, sendModLogMessage } from "../../../libs/discord-notifier";
 import { sendTagChangeEmail } from "../../../libs/mailer";
 import { getI18nFunctionByLanguage } from "../../../middleware/fetch-i18n";
+import { generateSecureCode } from "./connections";
+import sharp from "sharp";
+import Logger from "../../../libs/Logger";
 
 export function getCustomIconUrl(uuid: string, hash: string) {
     return `${config.baseUrl}/players/${uuid}/icon/${hash}`;
@@ -46,7 +49,7 @@ export default (app: ElysiaApp) => app.get('/:hash', async ({ params: { uuid, ha
     const player = await players.findOne({ uuid });
     if(session.equal && player?.isBanned()) return error(403, { error: i18n('error.banned') });
 
-    const isCustomIconDisallowed = snakeCase(GlobalIcon[GlobalIcon.Custom]) == icon && !session.hasPermission(Permission.CustomIcon);
+    const isCustomIconDisallowed = session.equal && snakeCase(GlobalIcon[GlobalIcon.Custom]) == icon && !session.hasPermission(Permission.CustomIcon);
     if(!session.hasPermission(Permission.BypassValidation) && (isCustomIconDisallowed || !(capitalCase(icon) in GlobalIcon) || config.validation.icon.blacklist.includes(capitalCase(icon)))) return error(403, { error: i18n('icon.notAllowed') });
 
     const oldIcon = player?.icon;
@@ -96,6 +99,46 @@ export default (app: ElysiaApp) => app.get('/:hash', async ({ params: { uuid, ha
         503: t.Object({ error: t.String() }, { description: 'The database is not reachable' })
     },
     body: t.Object({ icon: t.String({ error: 'error.wrongType;;[["field", "icon"], ["type", "string"]]' }) }, { error: 'error.invalidBody', additionalProperties: true }),
+    params: t.Object({ uuid: t.String({ description: 'Your UUID' }) }),
+    headers: t.Object({ authorization: t.String({ error: 'error.notAllowed', description: 'Your authentication token' }) }, { error: 'error.notAllowed' })
+}).post('/upload', async ({ session, body: { image }, params, i18n, error }) => { // Upload custom icon
+    if(!session || !session.equal) return error(403, { error: i18n('error.notAllowed') });
+
+    const player = await players.findOne({ uuid: stripUUID(params.uuid) });
+    if(!player) return error(404, { error: i18n('error.noTag') });
+    if(player.isBanned()) return error(403, { error: i18n('error.banned') });
+    if(!player.hasPermission(Permission.CustomIcon)) return error(403, { error: i18n('You\'re not allowed to have a custom icon!') });
+
+    const metadata = await sharp(await image.arrayBuffer()).metadata().catch((err: Error) => {
+        Logger.error('Failed to read image metadata:', err.message);
+        return null;
+    });
+
+    if(!metadata) return error(422, { error: i18n('The upload failed. Please contact support!') });
+    if(metadata.format != 'png') return error(422, { error: i18n('The file you uploaded is not a PNG image!')});
+    if(!metadata.height || metadata.height != metadata.width) return error(422, { error: i18n('The file you uploaded is not a square image!')});
+    if(metadata.height > config.validation.icon.maxResolution) return error(422, { error: i18n(`The file you uploaded exceeds the max resolution of ${config.validation.icon.maxResolution}x${config.validation.icon.maxResolution}!`) });
+
+    player.icon.name = snakeCase(GlobalIcon[GlobalIcon.Custom]);
+    player.icon.hash = generateSecureCode(32);
+    await player.save();
+    await Bun.write(Bun.file(join('icons', player.uuid, `${player.icon.hash}.png`)), await image.arrayBuffer(), { createPath: true });
+
+    return { message: i18n(`icon.success.${session.equal ? 'self' : 'admin'}`), hash: player.icon.hash };
+}, {
+    detail: {
+        tags: ['Settings'],
+        description: 'Upload a custom icon'
+    },
+    response: {
+        200: t.Object({ message: t.String(), hash: t.String() }, { description: 'The icon was uploaded' }),
+        403: t.Object({ error: t.String() }, { description: 'You\'re not allowed to change your icon' }),
+        404: t.Object({ error: t.String() }, { description: 'You don\'t have an account' }),
+        422: t.Object({ error: t.String() }, { description: 'You\'re lacking the validation requirements' }),
+        429: t.Object({ error: t.String() }, { description: 'You\'re ratelimited' }),
+        503: t.Object({ error: t.String() }, { description: 'The database is not reachable' })
+    },
+    body: t.Object({ image: t.File({ type: 'image/png', error: 'error.wrongType;;[["field", "image"], ["type", "png file"]]' }) }, { error: 'error.invalidBody', additionalProperties: true }),
     params: t.Object({ uuid: t.String({ description: 'Your UUID' }) }),
     headers: t.Object({ authorization: t.String({ error: 'error.notAllowed', description: 'Your authentication token' }) }, { error: 'error.notAllowed' })
 }).patch('/role-visibility', async ({ session, body: { visible }, params, i18n, error }) => { // Toggle role icon
