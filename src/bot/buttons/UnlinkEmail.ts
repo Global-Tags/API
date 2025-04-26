8a404a5c7c0123f0cb0023bd8d10bb7bc61ce7f5
RappyTV <rappytv@rappytv.com>
2025-04-26 21:43:00 +0200

    Send email unlink message when manually unlinking email

--- src/bot/buttons/UnlinkEmail.ts
+++ src/bot/buttons/UnlinkEmail.ts
@@ -2,7 +2,7 @@ import { ButtonInteraction, Message, GuildMember, EmbedBuilder, MessageFlags } f
 import Button from "../structs/Button";
 import players, { Player } from "../../database/schemas/players";
 import { colors } from "../bot";
-import { ModLogType, sendModLogMessage } from "../../libs/discord-notifier";
+import { ModLogType, sendEmailLinkMessage, sendModLogMessage } from "../../libs/discord-notifier";
 import { Permission } from "../../types/Permission";
 import { stripUUID } from "../../libs/game-profiles";
 
@@ -19,16 +19,19 @@ export default class UnlinkEmailButton extends Button {
         if(!target) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ Player not found!')], flags: [MessageFlags.Ephemeral] });
         if(!target.connections.email.address) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ This player does not have their email address linked!')], flags: [MessageFlags.Ephemeral] });
 
+        const oldAddress = target.connections.email.address;
         target.connections.email.address = null;
         await target.save();
 
+        const user = await target.getGameProfile();
         sendModLogMessage({
             logType: ModLogType.UnlinkConnection,
-            user: await target.getGameProfile(),
+            user,
             staff: await player.getGameProfile(),
             discord: true,
             type: 'email'
         });
+        sendEmailLinkMessage(user, oldAddress, false);
 
         interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription('✅ The address was successfully unlinked!')], flags: [MessageFlags.Ephemeral] });
     }
