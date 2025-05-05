7ce5b6f04f67bd726d5791a2dbd62b1e524bb28a
RappyTV <rappytv@rappytv.com>
2025-05-05 20:18:52 +0200

    Add v2 component support to finishActions button

--- src/bot/buttons/FinishActions.ts
+++ src/bot/buttons/FinishActions.ts
@@ -14,14 +14,24 @@ export default class FinishActionsButton extends Button {
 
     async trigger(interaction: ButtonInteraction, message: Message, member: GuildMember, player: Player) {
         if(!player.canManagePlayers()) return interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.error).setDescription('❌ You\'re not allowed to perform this action!')], flags: [MessageFlags.Ephemeral] });
+        await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
-        const row = ActionRowBuilder.from(message.components[0] as any) as ActionRowBuilder<ButtonBuilder>;
-        row.components.forEach(component => component.setDisabled(true));
+        if(message.flags.has(MessageFlags.IsComponentsV2)) {
+            const buttonRow = ActionRowBuilder.from(message.components.at(-1) as any) as ActionRowBuilder<ButtonBuilder>;
+            buttonRow.components.forEach(component => component.setDisabled(true));
+            buttonRow.components[1].setLabel(`Processed by ${(await player.getGameProfile()).username || member.user.username}`);
 
-        const embed = EmbedBuilder.from(message.embeds[0]);
-        embed.setFooter({ text: `Processed by ${(await player.getGameProfile()).username || member.user.username}`, iconURL: `https://laby.net/texture/profile/head/${stripUUID(player.uuid)}.png?size=1024&overlay` });
+            message.edit({ components: [...message.components.slice(0, -1), buttonRow] });
+        } else {
+            const row = ActionRowBuilder.from(message.components[0] as any) as ActionRowBuilder<ButtonBuilder>;
+            row.components.forEach(component => component.setDisabled(true));
+    
+            const embed = EmbedBuilder.from(message.embeds[0]);
+            embed.setFooter({ text: `Processed by ${(await player.getGameProfile()).username || member.user.username}`, iconURL: `https://laby.net/texture/profile/head/${stripUUID(player.uuid)}.png?size=1024&overlay` });
+    
+            message.edit({ embeds: [embed], components: [row] });
+        }
 
-        message.edit({ embeds: [embed], components: [row] });
-        interaction.reply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription('✅ Action completed!')], flags: [MessageFlags.Ephemeral] });
+        interaction.editReply({ embeds: [new EmbedBuilder().setColor(colors.success).setDescription('✅ Action completed!')] });
     }
 }
