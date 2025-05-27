fcab5912201fd89be211e439362b761390451701
RappyTV <rappytv@rappytv.com>
2025-05-27 17:27:49 +0200

    Use croner instead of cron

--- src/libs/cron-jobs.ts
+++ src/libs/cron-jobs.ts
@@ -1,58 +1,62 @@
-import { CronJob } from "cron";
 import { checkExpiredEntitlements } from "./entitlement-expiry";
 import { saveMetrics } from "./metrics";
 import Logger from "./Logger";
-import playerSchema, { resetMonthlyReferrals } from "../database/schemas/players";
+import playerSchema from "../database/schemas/players";
 import { config } from "./config";
 import { synchronizeRoles, updateRoleCache } from "../database/schemas/roles";
 import { isConnected } from "../database/mongo";
+import { Cron } from "croner";
 
 const tz = 'Europe/Berlin';
 
 export function startEntitlementExpiry() {
     if(!config.discordBot.notifications.entitlements.enabled) return;
     Logger.debug('Entitlement expiry initialized.');
-    new CronJob('*/5 * * * *', () => { setImmediate(() => {
-        Logger.debug('Start - checkExpiredEntitlements');
-        checkExpiredEntitlements();
-        Logger.debug('End - checkExpiredEntitlements');
-    }) }, null, false, tz, null, true).start();
+    const job = new Cron('*/5 * * * *', checkExpiredEntitlements, {
+        name: 'Entitlement Expiry Check',
+        timezone: tz
+    });
+    job.trigger();
 }
 
 export function startMetrics() {
     if(!config.metrics.enabled) return;
     Logger.debug('Metric initialized.');
-    new CronJob(config.metrics.cron, () => { setImmediate(() => {
-        Logger.debug('Start - saveMetrics');
-        saveMetrics();
-        Logger.debug('End - saveMetrics');
-    }) }, null, false, tz).start();
+    new Cron(config.metrics.cron, saveMetrics, {
+        name: 'Metrics Generator',
+        timezone: tz
+    });
 }
 
 export function startReferralReset() {
-    new CronJob('0 0 1 * *', () => {
-        setImmediate(async () => {
-            Logger.debug('Start - resetReferrals');
-            resetMonthlyReferrals();
-            Logger.debug('End - resetReferrals');
-        });
-    }, null, false, tz).start();
+    new Cron('0 0 1 * *', async () => {
+        if(!isConnected()) return;
+        const data = await playerSchema.find({ 'referrals.current_month': { $gt: 0 } });
+
+        for(const player of data) {
+            player.referrals.current_month = 0;
+            player.save();
+        }
+    }, {
+        name: 'Referral Resetter',
+        timezone: tz
+    });
 }
 
 export function startRoleCacheJob() {
-    new CronJob('*/30 * * * *', () => { setImmediate(() => {
-        Logger.debug('Start - updateRoleCache');
-        updateRoleCache();
-        Logger.debug('End - updateRoleCache');
-    }) }, null, false, tz, null, true).start();
+    const job = new Cron('*/30 * * * *', updateRoleCache, {
+        name: 'Role Cache Updater',
+        timezone: tz
+    });
+    job.trigger();
 }
 
 export function startRoleSynchronization() {
     if(!config.discordBot.syncedRoles.enabled) return;
-    Logger.debug('Role syncronization initialized.');
-    new CronJob('*/10 * * * *', () => { setImmediate(() => {
-        Logger.debug('Start - synchronizeRoles');
-        synchronizeRoles();
-        Logger.debug('End - synchronizeRoles');
-    }) }, null, false, tz, null, true).start();
+    Logger.info('Role syncronization initialized.');
+    const job = new Cron('*/10 * * * *', synchronizeRoles, {
+        name: 'Role Synchronization',
+        timezone: tz
+    });
+    job.trigger();
 }
